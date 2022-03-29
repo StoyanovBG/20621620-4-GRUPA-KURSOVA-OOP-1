31b3573215024205703fa7fbd954bcad3f3ccc5c
Yavor Stoyanov <35624863+StoyanovBG@users.noreply.github.com>
2022-03-29 12:37:58 +0300

    Delete BookExceptions.java

--- BookExceptions.java
+++
@@ -1,7 +0,0 @@
-public class BookExceptions extends Exception{
-    //reiting da ne e otricatelno
-    //da nqma prazni poleta
-    //godina na izdavane da e sled 100 B.C.
-    //unikalniq id da ne e otricatelno
-
-}
